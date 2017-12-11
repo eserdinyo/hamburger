ce7d8884582b4415c4b8fc2d5e77ddb19147bf8d
Muhammet ESER <muhammeteser29@gmail.com>
2017-12-11 14:32:50 +0300

    files added

---
+++ app.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.toggle').click(() => {
+        
+        $('.toggle__button').toggleClass('active');
+        $('.mobile-nav').toggleClass('menu-active');
+            
+    });
+});
+
